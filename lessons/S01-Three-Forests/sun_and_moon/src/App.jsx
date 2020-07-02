bea21b71f785b2ada1e933045045558c9e563352
Thataw Batun <tbatun02@gmail.com>
2020-07-01 20:44:33 -0400

    updated sun and moon

--- lessons/S01-Three-Forests/sun_and_moon/src/App.jsx
+++ lessons/S01-Three-Forests/sun_and_moon/src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 // Import my customm components
-import SunAndMoon from './SunAndMoon.jsx'
-
+import SunAndMoon from './SunAndMoon.jsx';
+import Trinity from './Trinity.jsx';
+import ButtonUp from './ButtonUp.jsx';
+import ButtonDown from './ButtonDown.jsx';
 // Create a component
 const App = () => {
+    
+    const [whichImage, whichImageSet] = useState("/images/moon.png");
+    
+    const handleDown = () => {
+        whichImageSet("/images/moon.png");
+
+    }
+
+    const handleUp = () => {
+        whichImageSet("/images/sun.png");
+        
+    }
+
     return (
         <div>App has changed
-             <SunAndMoon />   
+             <SunAndMoon whichImage ={ whichImage }/>  
+             
+             <ButtonDown onClick ={ handleDown }/> 
+             <ButtonUp onClick ={ handleUp }/>
+
+             <Trinity />
             </div>
     );
 
---
+++ lessons/S01-Three-Forests/sun_and_moon/src/ButtonDown.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ButtonDown = ({onClick}) => {
+    return (
+        <img onClick={ onClick } src="/images/down.png" alt="Button Down" />
+    );
+
+}
+export default ButtonDown;
