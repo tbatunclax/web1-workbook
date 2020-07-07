e07b780c52b79fdce07dacf371fc491c439903ac
Thataw Batun <tbatun02@gmail.com>
2020-07-06 22:51:50 -0400

    move folder

---
+++ SPA-In-Class/src/App/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+    return (
+        <FooterStyled>My footer</FooterStyled>
+    );
+}
+
+export default Footer;
+
+const FooterStyled = styled.footer`
+        background-color: teal;
+        padding: 10px;
+`;
