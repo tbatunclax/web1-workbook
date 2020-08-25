b48b7fb45d7842aad0c3e27708826b4e1e6c7fea
Thataw Batun <tbatun02@gmail.com>
2020-08-24 23:53:59 -0400

    copy back SPA app

---
+++ SPA-In-Class/build/precache-manifest.f8a832bb74e9f49594525b5358372d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7def1146a4af50ddc6be83002bd3180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af640c888c6b7b85c57",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "6af640c888c6b7b85c57",
+    "url": "/static/js/2.db407b43.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/static/js/2.db407b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af307a06e63650b696c0",
+    "url": "/static/js/main.3ab0b380.chunk.js"
+  },
+  {
+    "revision": "e4647a99a103a5ed28db",
+    "url": "/static/js/runtime-main.794fe084.js"
+  }
+]);
